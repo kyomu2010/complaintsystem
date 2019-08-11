9875f0bc5d2c83cb540d15a442618831c8e43675
Mark Smart <marksmart26@gmail.com>
2019-08-11 19:53:40 +0100

    initial commit to add files

---
+++ src/main/java/com/myGroup/complaintsystem/App.java
@@ -0,0 +1,13 @@
+package com.myGroup.complaintsystem;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
